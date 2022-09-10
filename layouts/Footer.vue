bab18b28f26caeb12616dd79bbe0387b41973d93
Jaime Chizavane <jaime.jcm@gmail.com>
2022-09-10 21:03:49 +0200

    Experiencia

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -5,10 +5,10 @@
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="single-footer-widget">
                         <div class="logo">
-                            <nuxt-link to="/"><img src="../assets/img/logo.png" alt="logo"></nuxt-link>
+                            <nuxt-link to="/"><img src="./assets/img/logo.png" alt="logo"></nuxt-link>
                         </div>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                        <p>Please note that our products are delivered within 4 days.</p>
                     </div>
                 </div>
 
@@ -43,10 +43,10 @@
                         <h3>Contact Us</h3>
 
                         <ul class="footer-contact-info">
-                            <li><i class="fas fa-map-marker-alt"></i> Location: 2750 Quadra Street Victoria, Canada</li>
+                            <li><i class="fas fa-map-marker-alt"></i> Location: 2750 Quadra Street Victoria, USA</li>
                             <li><i class="fas fa-phone"></i> Call Us: <a href="#">(+123) 456-7898</a></li>
-                            <li><i class="far fa-envelope"></i> Email Us: <a href="#">support@bolster.com</a></li>
-                            <li><i class="fas fa-fax"></i> Fax: <a href="#">+123456</a></li>
+                            <li><i class="far fa-envelope"></i> Email Us: <a href="#">support@8traville.com</a></li>
+                            
                         </ul>
                     </div>
                 </div>
@@ -57,7 +57,7 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-6 col-md-6">
-                        <p>Copyright &copy; 2020 Bolster. All Rights Reserved</p>
+                        <p>Copyright &copy; 2022 eKutiva. All Rights Reserved</p>
                     </div>
 
                     <div class="col-lg-6 col-md-6">
